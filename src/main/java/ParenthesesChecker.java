a659b124e8781ed6ee6d154439dd79fd3599ac60
Cem Gurhan <cemg@live.co.uk>
2022-04-28 21:55:55 +0100

    cleaned up code a bit

--- src/main/java/ParenthesesChecker.java
+++ src/main/java/ParenthesesChecker.java
@@ -10,20 +10,19 @@ public class ParenthesesChecker {
         Stack<Character> letterStack = new Stack();
         ArrayList<Character> parenthesesArrayOpen = new ArrayList<>();
         ArrayList<Character> parenthesesArrayClosed = new ArrayList<>();
-//        int openBracketCount = 0;
-//        int closeBracketCount = 0;
+
         for(int i=0;i<arrayLetters.length;i++){
             letterStack.push(arrayLetters[i]);
         }
         while(letterStack.empty()==false){
             if(letterStack.peek().equals('(')||letterStack.peek().equals('[')
             ||letterStack.peek().equals('{')||letterStack.peek().equals('<')){
-//                openBracketCount++;
+
                 parenthesesArrayOpen.add(letterStack.peek());
                 letterStack.pop();
             }else if(letterStack.peek().equals(')')||letterStack.peek().equals(']')
             ||letterStack.peek().equals('}')||letterStack.peek().equals('>')){
-//                closeBracketCount++;
+
                 parenthesesArrayClosed.add(letterStack.peek());
                 letterStack.pop();
             }else{
@@ -60,15 +59,4 @@ public class ParenthesesChecker {
     }
 
 
-//if(letterStack.peek().equals('(')){
-//        openBracketCount++;
-//
-//        }
-//        if(letterStack.peek().equals(')')){
-//        closeBracketCount++;
-//        }
-//        if((closeBracketCount%2==0&&openBracketCount%2==0&&closeBracketCount!=0&&openBracketCount!=0
-//        ||closeBracketCount+openBracketCount==2)
-//        ){
-//        return true;
-//        }
+
